4b06c03311f4aa952a1b2fbbb243ae30969d6c2a
hema omer <32086055+big-yoshi@users.noreply.github.com>
2021-09-13 18:50:16 +0300

    finished overlay

--- components/Project.vue
+++ components/Project.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row class="project mb-14 mt-14" justify="space-between">
-        <v-col class="d-flex align-center " cols="6">
+        <v-col class="d-flex align-center " lg="6">
             <v-card class="text-left  elevation-0 ">
                 <v-card-title>
                     <h1 class="mb-4">{{project.title}}</h1>
@@ -16,7 +16,7 @@
                 </v-card-actions>
             </v-card>
         </v-col>
-        <v-col cols="6">
+        <v-col lg="6">
             <v-row class="elevation-1 rounded-t-lg rounded-b-lg">
                 <v-col class="d-flex align-center rounded-t-lg" style="background:#E7E7E7">
                     <v-card class="pa-2 rounded-circle mr-1 ml-1 elevation-0 " style="background:red" tile></v-card>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="page">
+        <v-overlay z-index="999999999" :value="overlay">
+            <v-card >
+                <v-carousel v-model="model">
+                    <v-carousel-item v-for="(ui, i) in uis" :key="i" >
+                        <v-img :src="ui.img" contain>
+
+                        </v-img>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-card>
+            <v-btn @click="overlay = !overlay">Close</v-btn>
+        </v-overlay>
         <div class="landing">
             <v-img src="/white_eclipse.svg" class="eclipse" max-height="150px"></v-img>
             <v-row no-gutter class="text-center mt-14" align="center" justify="center">
@@ -53,8 +65,25 @@
         <div class="about-me">
             <h1>ss</h1>
         </div>
-        <div class="ui-portfolio">
-            <h2>skssk</h2>
+        <div class="ui-portfolio d-flex justify-center align-center mb-6 pa-2">
+            <v-row style="background:red" align="center" justify="center">
+                <v-col class="pa-1 ma-4 d-flex justify-center align-center" lg='3' xs='1'   style="background:pink" v-for="ui,i in uis" :key="i">
+                    <!-- <v-img :src="ui.img" max-height="300"></v-img> -->
+                    <v-hover>
+                        <template v-slot:default="{ hover }">
+                            <v-card class="mx-auto" >
+                                <v-img :src="ui.img" max-width="350"></v-img>
+                                <v-fade-transition>
+                                    <v-overlay v-if="hover" absolute color="#036358">
+                                        <v-btn @click="overlay = !overlay">See more info</v-btn>
+                                    </v-overlay>
+                                </v-fade-transition>
+
+                            </v-card>
+                        </template>
+                    </v-hover>
+                </v-col>
+            </v-row>
         </div>
         <div class="projects-portfolio pr-4 pl-4" align='center' justify="space-between">
             <Project v-for="project,i in projects" :key="i" :project="project"></Project>
@@ -74,11 +103,20 @@ export default {
     },
     data() {
         return {
+            model: 0,
+            
             projects: [
                 { title: 'CRM', tech: 'Bootsrap, PHP, Laravel', description: 'Customer relations project i made for SmartMed.com fully secure and responsive', source: '', live: '', img: 'https://camo.githubusercontent.com/0b89e43464db6e41ca59cb65d8ace2ea5a76f26940a5644ea7c82e7a17abf5b1/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f353637353133392f73637265656e73686f74732f31353631303431312f6d656469612f33633433633365386131646232316430666234343639626634643731313131312e6a70673f636f6d70726573733d3126726573697a653d38303078363030' },
                 { title: 'Ford UI', tech: 'Bootsrap, PHP, Laravel', description: 'Made this for my dream car', source: '', live: '', img: 'https://raw.githubusercontent.com/h3ma209/ui-designs-xd/main/adept/Web%201920%20%E2%80%93%203.png' },
                 { title: 'Mustang UI', tech: 'Bootsrap, PHP, Laravel', description: 'Made this for my dream car', source: '', live: '', img: 'https://cdn.dribbble.com/users/5675139/screenshots/15527090/media/1a6cf7d0bc3ba48442e948e754e99e2a.jpg?compress=1&resize=800x600' },
-            ]
+            ],
+            uis: [
+                { title: 'lorem ipsum', link: '', img: 'https://cdn.dribbble.com/users/5675139/screenshots/15527004/media/991a173559cb2938f3ccccc242a2095f.jpg?compress=1&resize=800x600' },
+                { title: 'lorem ipsum', link: '', img: 'https://cdn.dribbble.com/users/5675139/screenshots/15527004/media/991a173559cb2938f3ccccc242a2095f.jpg?compress=1&resize=800x600' },
+                { title: 'lorem ipsum', link: '', img: 'https://cdn.dribbble.com/users/5675139/screenshots/15527004/media/991a173559cb2938f3ccccc242a2095f.jpg?compress=1&resize=800x600' },
+                { title: 'lorem ipsum', link: '', img: 'https://cdn.dribbble.com/users/5675139/screenshots/15527004/media/991a173559cb2938f3ccccc242a2095f.jpg?compress=1&resize=800x600' },
+            ],
+            overlay: false,
         }
     }
 }
@@ -162,6 +200,15 @@ export default {
 
 .social-media i {
     font-size: 8vh;
+}
+/* UI ====+++=++=====++++++======================== */
+.ui-portfolio {
+}
+.ui-portfolio .row {
+    width: 80%;
+}
+.ui-portfolio .row .col {
+    position: relative;
 }
 
 /* Projects ======================================== */
